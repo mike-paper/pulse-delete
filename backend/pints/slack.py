3b1a4d71e64bea261838c78f60d57b2da6ec2020
mike-seekwell <34747639+mike-seekwell@users.noreply.github.com>
2021-07-26 08:27:50 -0400

    added slack oauth flow6

--- backend/pints/slack.py
+++ backend/pints/slack.py
@@ -30,15 +30,52 @@ def testPush():
     print('testPush result...', response["message"])
 
 def getToken(code):
-    authClient = WebClient()
-    response = authClient.oauth_v2_access(
+    client = WebClient()
+    oauth_response = client.oauth_v2_access(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri='https://pulse.trypaper.io/slack2',
         code=code
     )
-    logger.info(f'getToken... {response}')
-    return response
+    installed_enterprise = oauth_response.get("enterprise", {})
+    is_enterprise_install = oauth_response.get("is_enterprise_install")
+    installed_team = oauth_response.get("team", {})
+    installer = oauth_response.get("authed_user", {})
+    incoming_webhook = oauth_response.get("incoming_webhook", {})
+
+    bot_token = oauth_response.get("access_token")
+    # NOTE: oauth.v2.access doesn't include bot_id in response
+    bot_id = None
+    enterprise_url = None
+    if bot_token is not None:
+        auth_test = client.auth_test(token=bot_token)
+        bot_id = auth_test["bot_id"]
+        if is_enterprise_install is True:
+            enterprise_url = auth_test.get("url")
+
+    installation = {
+        'app_id': oauth_response.get("app_id"),
+        'enterprise_id': installed_enterprise.get("id"),
+        'enterprise_name': installed_enterprise.get("name"),
+        'enterprise_url': enterprise_url,
+        'team_id': installed_team.get("id"),
+        'team_name': installed_team.get("name"),
+        'bot_token': bot_token,
+        'bot_id': bot_id,
+        'bot_user_id': oauth_response.get("bot_user_id"),
+        'bot_scopes': oauth_response.get("scope"),  # comma-separated string
+        'user_id': installer.get("id"),
+        'user_token': installer.get("access_token"),
+        'user_scopes': installer.get("scope"),  # comma-separated string
+        'incoming_webhook_url': incoming_webhook.get("url"),
+        'incoming_webhook_channel': incoming_webhook.get("channel"),
+        'incoming_webhook_channel_id': incoming_webhook.get("channel_id"),
+        'incoming_webhook_configuration_url': incoming_webhook.get("configuration_url"),
+        'is_enterprise_install': is_enterprise_install,
+        'token_type': oauth_response.get("token_type"),
+    }
+    logger.info(f'getToken... {installation}')
+    return installation
 
 def push(d):
     logger.info(f'push...')
